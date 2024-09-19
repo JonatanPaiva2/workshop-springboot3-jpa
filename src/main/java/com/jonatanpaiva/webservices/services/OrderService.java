5d9870de23c0e55da86c76e6ccc4dd3a82e257c0
Jonatan Paiva <perrolha@gmail.com>
2024-09-19 15:40:53 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/jonatanpaiva/webservices/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.jonatanpaiva.webservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonatanpaiva.webservices.entities.Order;
+import com.jonatanpaiva.webservices.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+
+}
