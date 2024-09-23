42ceb083215445fb3d51cd5ba0a36cf6a0da99bc
Jonatan Paiva <perrolha@gmail.com>
2024-09-23 15:09:07 -0300

    Exception handling - findById

--- src/main/java/com/jonatanpaiva/webservices/services/UserService.java
+++ src/main/java/com/jonatanpaiva/webservices/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.jonatanpaiva.webservices.entities.User;
 import com.jonatanpaiva.webservices.repositories.UserRepository;
+import com.jonatanpaiva.webservices.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
